f999ea9f9395dee47c3be81a31d5d3e016f53c30
wouterlucas <wouter@wouterlucas.com>
2016-02-18 21:58:31 -0800

    tools: added libprovision to buildroot sync script

--- tools/sync_from_builtroot.sh
+++ tools/sync_from_builtroot.sh
@@ -9,6 +9,7 @@ wget -q https://github.com/Metrological/buildroot-wpe/raw/master/package/wpe/wpe
 wget -q https://github.com/Metrological/buildroot-wpe/raw/master/package/wpe-launcher/wpe-launcher.mk
 wget -q https://raw.githubusercontent.com/Metrological/buildroot-wpe/master/package/cppsdk/cppsdk.mk
 wget -q https://github.com/Metrological/buildroot-wpe/raw/master/package/dxdrm/dxdrm.mk
+wget -q https://github.com/Metrological/buildroot-wpe/raw/master/package/libprovision/libprovision.mk
 wget -q https://github.com/Metrological/buildroot-wpe/raw/master/package/webbridge/webbridge.mk
 wget -q https://github.com/Metrological/buildroot-wpe/raw/master/package/netflix/netflix.mk
 cd ..
@@ -18,6 +19,7 @@ python ./tools/check_srcrevs.py -i ./tmp/wpe.mk -o ./recipes-wpe/wpe/wpe_0.1.bb
 python ./tools/check_srcrevs.py -i ./tmp/wpe-launcher.mk -o ./recipes-wpe/wpe-launcher/wpe-launcher_0.1.bb
 python ./tools/check_srcrevs.py -i ./tmp/cppsdk.mk -o ./recipes-middleware/cppsdk/cppsdk_git.bb
 python ./tools/check_srcrevs.py -i ./tmp/dxdrm.mk -o ./recipes-middleware/dxdrm/dxdrm_git.bb
+python ./tools/check_srcrevs.py -i ./tmp/libprovision.mk -o ./recipes-middleware/libprovision/libprovision_git.bb
 python ./tools/check_srcrevs.py -i ./tmp/webbridge.mk -o ./recipes-middleware/webbridge/webbridge_git.bb
 python ./tools/check_srcrevs.py -i ./tmp/netflix.mk -o ./recipes-netflix/netflix/netflix_git.bb
 
