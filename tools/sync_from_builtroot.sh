653904f30aaf2daf6c957fe0d173131a23b312c3
wouterlucas <wouter@wouterlucas.com>
2016-10-09 14:28:27 -0700

    sync script: add webdriver sync

--- tools/sync_from_builtroot.sh
+++ tools/sync_from_builtroot.sh
@@ -13,6 +13,7 @@ wget -q https://github.com/Metrological/buildroot-wpe/raw/master/package/libprov
 wget -q https://github.com/Metrological/buildroot-wpe/raw/master/package/webbridge/webbridge.mk
 wget -q https://github.com/Metrological/buildroot-wpe/raw/master/package/netflix/netflix.mk
 wget -q https://github.com/Metrological/buildroot-wpe/raw/master/package/icudata/icudata.mk
+wget -q https://github.com/Metrological/buildroot-wpe/raw/master/package/webdriver/webdriver.mk
 
 # QT5 webkit
 wget -q https://raw.githubusercontent.com/Metrological/buildroot/master/package/qt5/qt5webkit/qt5webkit.mk
@@ -28,6 +29,7 @@ python ./tools/check_srcrevs.py -i ./tmp/libprovision.mk -o ./recipes-metrologic
 python ./tools/check_srcrevs.py -i ./tmp/webbridge.mk -o ./recipes-metrological/webbridge/webbridge_git.bb
 python ./tools/check_srcrevs.py -i ./tmp/netflix.mk -o ./recipes-netflix/netflix/netflix_git.bb
 python ./tools/check_srcrevs.py -i ./tmp/icudata.mk  -o ./recipes-support/icu/icu_56%.bbappend
+python ./tools/check_srcrevs.py -i ./tmp/webdriver.mk  -o ./recipes-metrological/webdriver/webdriver-wpe_git.bb
 
 python ./tools/check_srcrevs.py -i ./tmp/qt5webkit.mk  -o ./recipes-qt/qt5/qtwebkit_metro.bb
 
