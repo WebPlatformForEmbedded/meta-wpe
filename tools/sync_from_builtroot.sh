dfe302da1571627a980ef43b7fd6ef868e508d1f
wouterlucas <wouter@wouterlucas.com>
2017-02-01 14:50:00 -0800

    tools: Add opencdm and ocdmi to BR sync script

--- tools/sync_from_builtroot.sh
+++ tools/sync_from_builtroot.sh
@@ -13,6 +13,8 @@ wget -q https://github.com/Metrological/buildroot-wpe/raw/master/package/webbrid
 wget -q https://github.com/Metrological/buildroot-wpe/raw/master/package/netflix/netflix.mk
 wget -q https://github.com/Metrological/buildroot-wpe/raw/master/package/icudata/icudata.mk
 wget -q https://github.com/Metrological/buildroot-wpe/raw/master/package/webdriver/webdriver.mk
+wget -q https://github.com/Metrological/buildroot-wpe/raw/master/package/opencdm/opencdm.mk
+wget -q https://github.com/Metrological/buildroot-wpe/raw/master/package/opencdmi/opencdmi.mk
 
 # QT5 webkit
 wget -q https://raw.githubusercontent.com/Metrological/buildroot/master/package/qt5/qt5webkit/qt5webkit.mk
@@ -26,10 +28,12 @@ python ./tools/check_srcrevs.py -i ./tmp/cppsdk.mk -o ./recipes-metrological/cpp
 python ./tools/check_srcrevs.py -i ./tmp/libprovision.mk -o ./recipes-metrological/libprovision/libprovision_git.bb
 python ./tools/check_srcrevs.py -i ./tmp/webbridge.mk -o ./recipes-metrological/webbridge/webbridge_git.bb
 python ./tools/check_srcrevs.py -i ./tmp/netflix.mk -o ./recipes-netflix/netflix/netflix_git.bb
-python ./tools/check_srcrevs.py -i ./tmp/icudata.mk  -o ./recipes-support/icu/icu_56%.bbappend
-python ./tools/check_srcrevs.py -i ./tmp/webdriver.mk  -o ./recipes-metrological/webdriver/webdriver-wpe_git.bb
+python ./tools/check_srcrevs.py -i ./tmp/icudata.mk -o ./recipes-support/icu/icu_56%.bbappend
+python ./tools/check_srcrevs.py -i ./tmp/webdriver.mk -o ./recipes-metrological/webdriver/webdriver-wpe_git.bb
+python ./tools/check_srcrevs.py -i ./tmp/opencdm.mk -o ./recipes-drm/opencdm/opencdm_git.bb
+python ./tools/check_srcrevs.py -i ./tmp/opencdmi.mk -o ./recipes-drm/opencdmi/opencdmi_git.bb
 
-python ./tools/check_srcrevs.py -i ./tmp/qt5webkit.mk  -o ./recipes-qt/qt5/qtwebkit_metro.bb
+python ./tools/check_srcrevs.py -i ./tmp/qt5webkit.mk -o ./recipes-qt/qt5/qtwebkit_metro.bb
 
 echo "Cleaning up tmp"
 rm -r ./tmp
