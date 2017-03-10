284e0b8d241ba868ad8063e974160f34ac44c5ed
wouterlucas <wouter@wouterlucas.com>
2017-03-10 13:00:02 -0800

    tools: Adjust sync script and add rpi yocto manifest

--- tools/sync_from_builtroot.sh
+++ tools/sync_from_builtroot.sh
@@ -5,35 +5,32 @@ mkdir ./tmp
 cd tmp
 
 echo "Fetching buildroot make files"
-wget -q https://github.com/Metrological/buildroot-wpe/raw/master/package/wpe/wpe.mk
-wget -q https://github.com/Metrological/buildroot-wpe/raw/master/package/wpe-launcher/wpe-launcher.mk
-wget -q https://raw.githubusercontent.com/Metrological/buildroot-wpe/master/package/cppsdk/cppsdk.mk
-wget -q https://github.com/Metrological/buildroot-wpe/raw/master/package/libprovision/libprovision.mk
-wget -q https://github.com/Metrological/buildroot-wpe/raw/master/package/webbridge/webbridge.mk
-wget -q https://github.com/Metrological/buildroot-wpe/raw/master/package/netflix/netflix.mk
-wget -q https://github.com/Metrological/buildroot-wpe/raw/master/package/icudata/icudata.mk
-wget -q https://github.com/Metrological/buildroot-wpe/raw/master/package/webdriver/webdriver.mk
-wget -q https://github.com/Metrological/buildroot-wpe/raw/master/package/opencdm/opencdm.mk
-wget -q https://github.com/Metrological/buildroot-wpe/raw/master/package/opencdmi/opencdmi.mk
-
-# QT5 webkit
-wget -q https://raw.githubusercontent.com/Metrological/buildroot/master/package/qt5/qt5webkit/qt5webkit.mk
+wget -q https://github.com/WebPlatformForEmbedded/buildroot/raw/master/package/wpe/wpebackend/wpebackend.mk
+wget -q https://github.com/WebPlatformForEmbedded/buildroot/raw/master/package/wpe/wpewebkit/wpewebkit.mk
+wget -q https://github.com/WebPlatformForEmbedded/buildroot/raw/master/package/wpe/wpelauncher/wpelauncher.mk
+wget -q https://github.com/WebPlatformForEmbedded/buildroot/raw/master/package/cppsdk/cppsdk.mk
+wget -q https://github.com/WebPlatformForEmbedded/buildroot/raw/master/package/libprovision/libprovision.mk
+wget -q https://github.com/WebPlatformForEmbedded/buildroot/raw/master/package/webbridge/webbridge.mk
+wget -q https://github.com/WebPlatformForEmbedded/buildroot/raw/master/package/netflix/netflix.mk
+wget -q https://github.com/WebPlatformForEmbedded/buildroot/raw/master/package/icudata/icudata.mk
+wget -q https://github.com/WebPlatformForEmbedded/buildroot/raw/master/package/webdriver/webdriver.mk
+wget -q https://github.com/WebPlatformForEmbedded/buildroot/raw/master/package/opencdm/opencdm.mk
+wget -q https://github.com/WebPlatformForEmbedded/buildroot/raw/master/package/opencdmi/opencdmi.mk
 
 cd ..
 
 echo "Checking and updating revs"
-python ./tools/check_srcrevs.py -i ./tmp/wpe.mk -o ./recipes-wpe/wpe/wpe_0.1.bb
-python ./tools/check_srcrevs.py -i ./tmp/wpe-launcher.mk -o ./recipes-wpe/wpe-launcher/wpe-launcher_0.1.bb
+python ./tools/check_srcrevs.py -i ./tmp/wpebackend.mk -o ./recipes-wpe/wpebackend/wpebackend_0.1.bb
+python ./tools/check_srcrevs.py -i ./tmp/wpewebkit.mk -o ./recipes-wpe/wpewebkit/wpewebkit_0.1.bb
+python ./tools/check_srcrevs.py -i ./tmp/wpelauncher.mk -o ./recipes-wpe/wpelauncher/wpelauncher_0.1.bb
 python ./tools/check_srcrevs.py -i ./tmp/cppsdk.mk -o ./recipes-metrological/cppsdk/cppsdk_git.bb
 python ./tools/check_srcrevs.py -i ./tmp/libprovision.mk -o ./recipes-metrological/libprovision/libprovision_git.bb
 python ./tools/check_srcrevs.py -i ./tmp/webbridge.mk -o ./recipes-metrological/webbridge/webbridge_git.bb
 python ./tools/check_srcrevs.py -i ./tmp/netflix.mk -o ./recipes-netflix/netflix/netflix_git.bb
-python ./tools/check_srcrevs.py -i ./tmp/icudata.mk -o ./recipes-support/icu/icu_56%.bbappend
+python ./tools/check_srcrevs.py -i ./tmp/icudata.mk -o ./recipes-support/icu/icu_57%.bbappend
 python ./tools/check_srcrevs.py -i ./tmp/webdriver.mk -o ./recipes-metrological/webdriver/webdriver-wpe_git.bb
 python ./tools/check_srcrevs.py -i ./tmp/opencdm.mk -o ./recipes-drm/opencdm/opencdm_git.bb
 python ./tools/check_srcrevs.py -i ./tmp/opencdmi.mk -o ./recipes-drm/opencdmi/opencdmi_git.bb
-
-python ./tools/check_srcrevs.py -i ./tmp/qt5webkit.mk -o ./recipes-qt/qt5/qtwebkit_metro.bb
 
 echo "Cleaning up tmp"
 rm -r ./tmp
