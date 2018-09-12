WebPlatformForEmbedded/meta-wpe

commit 129c8c64821902cc311084cdb9251d39e7a3f8e2

wpeframework-plugins: align remote control changes

recipes-wpe/wpeframework/wpeframework-plugins_git.bb
@@ -25,8 +25,6 @@ WEBKITBROWSER_TRANSPARENT ?= "false"
 WEBKITBROWSER_THREADEDPAINTING ?= "1"
 
 WPEFRAMEWORK_LOCATIONSYNC_URI ?= "http://jsonip.metrological.com/?maf=true"
-WPEFRAMEWORK_REMOTECONTROL_NEXUS_IRMODE ?= "16"
-WPEFRAMEWORK_REMOTECONTROL_KEYMAP = "OSMCKeyMap.json"
 
 WPEFRAMEWORK_PLUGIN_WEBSERVER_PORT ?= "8080"
 WPEFRAMEWORK_PLUGIN_WEBSERVER_PATH ?= "/var/www/"
@@ -82,12 +80,10 @@ PACKAGECONFIG[opencdmi_pr]    = "-DPLUGIN_OPENCDMI_PLAYREADY=ON,,"
 PACKAGECONFIG[opencdmi_prnx]  = "-DPLUGIN_OPENCDMI_PLAYREADY_NEXUS=ON,,"
 PACKAGECONFIG[opencdmi_wv]    = "-DPLUGIN_OPENCDMI_WIDEVINE=ON,,"
 PACKAGECONFIG[remote]         = "-DWPEFRAMEWORK_PLUGIN_REMOTECONTROL=ON \
-                                 -DWPEFRAMEWORK_PLUGIN_REMOTECONTROL_KEYMAP=${WPEFRAMEWORK_REMOTECONTROL_KEYMAP} \
                                 ,-DWPEFRAMEWORK_PLUGIN_REMOTECONTROL=OFF,"
-PACKAGECONFIG[remote-nexus]   = "-DWPEFRAMEWORK_PLUGIN_REMOTECONTROL_IRNEXUS=ON \
-                                 -DWPEFRAMEWORK_PLUGIN_REMOTECONTROL_IRMODE=${WPEFRAMEWORK_REMOTECONTROL_IRMODE} \
-                                ,-DWPEFRAMEWORK_PLUGIN_REMOTECONTROL_IRNEXUS=OFF,broadcom-refsw"
-PACKAGECONFIG[remote-uinput]  = "-DWPEFRAMEWORK_PLUGIN_REMOTECONTROL_DEVINPUT=ON,-DWPEFRAMEWORK_PLUGIN_REMOTECONTROL_DEVINPUT=OFF,"
+PACKAGECONFIG[remote-ir]      = "-DWPEFRAMEWORK_PLUGIN_REMOTECONTROL_IR=ON \
+                                ,-DWPEFRAMEWORK_PLUGIN_REMOTECONTROL_IR=OFF,"
+PACKAGECONFIG[remote-devinput] = "-DWPEFRAMEWORK_PLUGIN_REMOTECONTROL_DEVINPUT=ON,-DWPEFRAMEWORK_PLUGIN_REMOTECONTROL_DEVINPUT=OFF,"
 PACKAGECONFIG[remote-gp]      = "-DWPEFRAMEWORK_PLUGIN_REMOTECONTROL_RFCE_NAME=${REMOTECONTROL_RF4CE_NAME} \
 				 -DWPEFRAMEWORK_PLUGIN_REMOTECONTROL_RFCE_REMOTE_ID=${REMOTECONTROL_RF4CE_REMOTE_ID} \ 
 				 -DWPEFRAMEWORK_PLUGIN_REMOTECONTROL_RFCE_MODULE=${REMOTECONTROL_RF4CE_MODULE} \

commit d6b81be3afabb2a9094a79ea73a97d77122aa433

wpeframework-plugins: Fix 'devinput' in default PACKAGECONFIG
Commit 129c8c648219 ("wpeframework-plugins: align remote control
changes") changed the PACKAGECONFIG option name for enabling the
'devinput' remote control device from "remote-uinput" to
"remote-devinput".

However, the previous option name "remote-uinput" was left in the
default value for the plugins PACKAGECONFIG, which causes the 'devinput'
device no no longer be enabled by default. Fix this by using the new
name there as well.

Fixes: 129c8c648219 ("wpeframework-plugins: align remote control changes")
Signed-off-by: Ricardo Silva <ricardo.silva@gbtembedded.com>

recipes-wpe/wpeframework/wpeframework-plugins_git.bb
@@ -55,7 +55,7 @@ PACKAGECONFIG ?= " \
     ${@bb.utils.contains('DISTRO_FEATURES', 'bluetooth', 'bluetooth', '', d)} \
     ${@bb.utils.contains('DISTRO_FEATURES', 'wpeframework', '${WPE_COMPOSITOR} network', '', d)} \
     ${@bb.utils.contains('MACHINE_FEATURES', 'wifi', 'network wifi', '', d)} \
-    deviceinfo dictionary locationsync monitor remote remote-uinput timesync tracing ux virtualinput webkitbrowser webserver youtube \
+    deviceinfo dictionary locationsync monitor remote remote-devinput timesync tracing ux virtualinput webkitbrowser webserver youtube \
 "
 
 PACKAGECONFIG[bluetooth]      = "-DWPEFRAMEWORK_PLUGIN_BLUETOOTH=ON -DWPEFRAMEWORK_PLUGIN_BLUETOOTH_AUTOSTART=false,-DWPEFRAMEWORK_PLUGIN_BLUETOOTH=OFF,,dbus-glib bluez5"