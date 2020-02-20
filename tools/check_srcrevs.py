769748fc0d22c719a669c672ea4876a29f82615b
Ansu <ansu.m@tataelxsi.co.in>
2020-02-20 18:31:14 +0530

    BD-786 : Request To Scan Metrological Github meta-wpe

--- tools/check_srcrevs.py
+++ tools/check_srcrevs.py
@@ -1,3 +1,20 @@
+# If not stated otherwise in this file or this component's license file the
+# following copyright and licenses apply:
+#
+# Copyright 2020 RDK Management
+#
+# Licensed under the Apache License, Version 2.0 (the License);
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an AS IS BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import sys, getopt, re, fileinput
 
 def findRev(file):
@@ -10,18 +27,18 @@ def findRev(file):
 
 def updateOeVersion(file, newVersion):
     # Read contents from file as a single string
-    file_handle = open(file, 'r')
-    file_string = file_handle.read()
-    file_handle.close()
+    fd = open(file, 'r')
+    file_content = fd.read()
+    fd.close()
 
     # Use RE package to allow for replacement (also allowing for (multiline) REGEX)
-    file_string = (re.sub("[a-z0-9]{40}", newVersion, file_string))
+    file_content = (re.sub("[a-z0-9]{40}", newVersion, file_content))
 
     # Write contents to file.
     # Using mode 'w' truncates the file.
-    file_handle = open(file, 'w')
-    file_handle.write(file_string)
-    file_handle.close()	
+    fd = open(file, 'w')
+    fd.write(file_content)
+    fd.close()	
 
 def main(argv):
 	inputfile = ''
